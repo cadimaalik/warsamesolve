776a137be400754aaf5532e829c45c26a0b193a0
firatce <karafenerjb@gmail.com>
2026-03-01 15:48:53 +0300

    Revert "feat: add Function Approximation calculator (CE 305 Chapter 5)"

--- funcapprox/src/hooks/useMathJax.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function useMathJax(deps = []) {
-  useEffect(() => {
-    if (window.MathJax && window.MathJax.typesetPromise) {
-      window.MathJax.typesetPromise().catch(() => {});
-    }
-  }, deps);
-}
